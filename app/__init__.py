42d9a0cad76e8b7da0d9903a506c555b52fbe661
smtsuchi <shoha1999@gmail.com>
2022-07-26 18:44:34 -0400

    end of monday

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config
 
+# import blueprints
+from .auth.routes import auth
+
 app = Flask(__name__)
+
+
+# register blueprints
+app.register_blueprint(auth)
 
 app.config.from_object(Config)
 
