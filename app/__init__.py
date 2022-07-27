f7978005094680d13bc71fee656e2ca1d6e42827
smtsuchi <shoha1999@gmail.com>
2022-07-26 21:48:04 -0400

    tuesday

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from config import Config
+from flask_migrate import Migrate
 
 # import blueprints
 from .auth.routes import auth
 
 app = Flask(__name__)
 
-
 # register blueprints
 app.register_blueprint(auth)
 
 app.config.from_object(Config)
 
+# initialize our database to work with our app
+from .models import db
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+
 from . import routes
+from . import models
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,12 +1,49 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from .forms import UserCreationForm
+
+# import models
+from app.models import User
 
 auth = Blueprint('auth', __name__, template_folder='authtemplates')
+
+from app.models import db
 
 @auth.route('/login')
 def logMeIn():
     return render_template('login.html')
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=["GET", "POST"])
 def signMeUp():
-    return render_template('login.html')
+    form = UserCreationForm()
+    if request.method == "POST":
+        print('POST request made')
+        if form.validate():
+            username = form.username.data
+            email = form.email.data
+            password = form.password.data
+
+            username=='pikachu'
+
+            import requests
+
+            
+            requests.get(f'pokeapi.co/v1/pokemon/{username}')
+
+
+            print(username, email, password)
+
+            # add user to database
+            user = User(username, email, password)
+
+            # add instance to our db
+            db.session.add(user)
+            db.session.commit()
+
+            return redirect(url_for('auth.logMeIn'))
+        else:
+            print('validation failed')
+    else:
+        print('GET req made')
+
+    return render_template('signup.html', form = form)
